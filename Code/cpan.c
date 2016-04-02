4e29769dc42c1dab326838d4552c6ff8401f8589
Steve Pedersen <stevedpedersen@gmail.com>
2016-04-01 17:14:13 -0700

    working linear interpolation of dummy data set

--- Code/cpan.c
+++ Code/cpan.c
@@ -14,6 +14,7 @@
 #include <string.h>
 #include "header.h"
 #define P printf
+#define N 9
 int anread(char*, int);             /* 05/06 /96 */
 
 /*    global variables declared as externs in monan.h need a root position  */
@@ -24,15 +25,15 @@ float *cmag, *dfr, *phase, *br, *time, tl, dt, fa, smax, *newmag, *newfr,
 double ampscale;
 
 float *w, *a;
-int   *b, brk, frames, nbreaks;
+int   *b, LR[2], brk, frames;
 
-int envelope();
-void interpolate(int brindex);
-int compare_ints (const void *a, const void *b);
+int findBreak();
+void findLR(int Nbk);
+void interpolate(int L, int R);
 
 int main(int argc, char **argv)
 {
-  int i,j,k,harms;
+  int i,j,k,harms,Nbk;
 
 /* read in an analysis file */
   anread(argv[1],-1);
@@ -40,95 +41,88 @@ int main(int argc, char **argv)
   printf("# harmonics = %d # timepoints = %d\n",nhar, npts);
   //frames = npts;
   harms = 1;
-  nbreaks = 5;
   frames = 18; 
-
+  if (argc == 3) 
+    Nbk = atoi(argv[2]);
+  else
+    Nbk = N;
   // dummy data for "actual" points
   static const float DUMMY[] = {0,17,15,18,8,9,16,15,12,14,11,7,9,8,4,1,2,0};
 
   a = (float *) calloc(frames,sizeof(float));
   w = (float *) calloc(frames,sizeof(float));
-  b = (int *) calloc(nbreaks,sizeof(int));
+  b = (int *) calloc(Nbk,sizeof(int));
 
   memcpy(a, DUMMY, sizeof(DUMMY));
-  memset(b, 2000, nbreaks * sizeof(int));
+  
+  // save startpoint as the first break point
+  for (i=1;i<Nbk;i++) { b[i]=-1; }
+  b[0]=0; brk=0;
 
   // for each harmonic
   for (k=1;k<=harms;k++) {
-    // set all working points to zero
+    
     for (i=0;i<frames-1;i++) {
-      w[i] = 0;
-    }
-    // save startpoint frame #'s as the first break points
-    b[0]=0; brk=0; 
-      
-    for (j=1;j<=nbreaks;j++) {
-      // TODO: 2nd param to change to "frames" or "npts"
-      b[j] = envelope();
-      qsort(b,nbreaks,sizeof (int),compare_ints);
-      //for(i=0;i<nbreaks;i++){P("%d\n",b[i]);}
-      interpolate(j);
+      w[i] = 0; // set all working points to zero
+    }    
+    //P("i%d %4d\n",0,b[0]);  
+    for (i=1;i<Nbk;i++) {
+      b[i] = findBreak();
+      //P("i%d %4d\n",i,b[i]);
+      findLR(Nbk);
+      interpolate(LR[0], b[i]);
+      interpolate(b[i], LR[1]);
     }
   }
-  // P("working amps\n");
-  // for (i=0;i<frames;i++) {
-  //   P("w%.2d %4.2f\n",i,w[i]);
+  // for (i=0;i<Nbk;i++) {
+  //   P("i%d %4d\n",i,b[i]);
   // }
-
+  P("working amps\n");
+  for (i=0;i<frames;i++) {
+    P("w%.2d %4.2f\n",i,w[i]);
+  }
   free(a); free(w); free(b);
 }
 
-int envelope() {
+int findBreak() {
   int i; float max=0;          
-  // find new brkpt w/ largest error
-  for (i=1;i<frames-1;i++) {
-    if (fabs(w[i]-a[i]) >= max) {
-      //P("w-a=%f\n",fabs(w[i]-a[i]));
+  
+  for (i=1;i<frames;i++) {
+    if (fabs(w[i]-a[i]) > max) {
       max = a[i];
       brk = i;
     }   
   }
   w[brk] = max;
-  P("working amps\n");
-  for (i=0;i<frames;i++) {
-    P("w%.2d %4.2f\n",i,w[i]);
-  }
   return brk;
 }
-  
-void interpolate(int brindex) {
-  int x, i,j, x1, x2, temp;
-  float y1, y2;
-
-  for (i=0;i<nbreaks;i++) {
-    P("\nbi=%d\n",b[i]);
-    if (b[i]>1999) {
-      P("\n>1999\n");
-      x1=x2; y1=w[x2];
-      x2=frames-1;  y2=0;
-    }
-    if (b[i] == 0) {
-      x1=0; y1=w[x1];
-      x2=b[i+1]; y2=w[x2];
-    } else if (y2 != 0) {
-      x1=b[i];  y1=w[x1];
-      x2=b[i+1];y2=w[x2];
-    }   
-    for (j=x1;j<x2;j++) {
-      x=j;
-      if (x>=x1 && x<=x2) {
-        w[j]=y1 + (y2-y1) * (x - x1)/(x2 - x1);
-      }
-    }
-  }
  
+void findLR (int Nbk) {
+  int i, L=0, R=9999;
+  for (i=0;i<Nbk;i++) {
+    if ((i==Nbk-1) && (R==9999)) {
+      R = frames-1;
+    } else 
+    if (b[i] < brk) {
+      if (b[i] >= L) { L = b[i]; }
+      //else if (b[i] < 0) { R = frames-1; }
+    }
+    if (b[i] > brk) {
+      if (b[i] < R) { R = b[i]; }
+      else if (b[i] < 0) { R = frames-1; }
+    } 
+  }
+  LR[0] = L; LR[1] = R;
+  //P("L=%d, brk=%d, R=%d\n",L,brk,R); 
+  //P("brk=%d\n",b[brk]); 
 }
 
-int
-compare_ints (const void *a, const void *b)
-{
-  const int *da = (const int *) a;
-  const int *db = (const int *) b;
+void interpolate(int L, int R) {
+  int i,x, x1=L, x2=R;
+  float y1=w[x1], y2=w[x2];
 
-  return (*da > *db) - (*da < *db);
+  for (i=x1;i<=x2;i++) {
+    x=i;
+    w[i]=y1 + (y2-y1) * (x - x1)/(x2 - x1);
+  }
 }
